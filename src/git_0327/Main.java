25af8df89326a53020624fa0fa656f682b2373f0
Ju, Hyeonji <hyeonji443@gmail.com>
2024-09-25 00:49:13 +0900

    sixth

--- src/git_0327/Main.java
+++ src/git_0327/Main.java
@@ -1,9 +1,11 @@
 package git_0327;
 
-public static void main(String[] args) {
-    System.out.println("주현지");
-    System.out.println("하나");
-  }
+public class Main {
 
+	  public static void main(String[] args) {
+	    System.out.println("주현지");
+	    System.out.println("하나");
+	    System.out.println("둘");
+	  }
 
-}
+	}
